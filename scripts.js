41760b35be50b3bd3033f5495e37cb985020a8c4
Anurag Kakustham <kakustamanurag@gmail.com>
2020-11-05 16:21:56 +0530

    Added the play on window click script

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-import adapter from 'webrtc-adapter';
+AFRAME.registerComponent('play-on-window-click', {
+    init: function () {
+      this.onClick = this.onClick.bind(this);
+    },
+    play: function () {
+      window.addEventListener('click', this.onClick);
+    },
+    pause: function () {
+      window.removeEventListener('click', this.onClick);
+    },
+    onClick: function (evt) {
+      var video = this.el.components.material.material.map.image;
+      if (!video) { return; }
+      video.play();
+    }
+  });
